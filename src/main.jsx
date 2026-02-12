ba40f0e6035ff676dbb9e8d32be89d9f20336eb1
swati mali <swati.mali912@gmail.com>
2026-02-12 10:29:07 +0530

    Added required project images to assets folder

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRoter} from 'react-router-dom'
+import{AppContextProvider} from './context/AppContect.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
